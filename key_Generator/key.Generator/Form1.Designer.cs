778a593c318384d3c242646069fd1235868fe11a
FATİH KOÇ <128058042+therandomaccess@users.noreply.github.com>
2023-03-29 16:39:54 +0300

    Key_Generator
    c#

---
+++ key_Generator/key.Generator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace key.Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.length = new System.Windows.Forms.Label();
+            this.check_ABC = new System.Windows.Forms.CheckBox();
+            this.check_abcd = new System.Windows.Forms.CheckBox();
+            this.check_123 = new System.Windows.Forms.CheckBox();
+            this.check_ask = new System.Windows.Forms.CheckBox();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_length = new System.Windows.Forms.Label();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // length
+            // 
+            resources.ApplyResources(this.length, "length");
+            this.length.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.length.Name = "length";
+            // 
+            // check_ABC
+            // 
+            resources.ApplyResources(this.check_ABC, "check_ABC");
+            this.check_ABC.Name = "check_ABC";
+            this.check_ABC.UseVisualStyleBackColor = true;
+            // 
+            // check_abcd
+            // 
+            resources.ApplyResources(this.check_abcd, "check_abcd");
+            this.check_abcd.Name = "check_abcd";
+            this.check_abcd.UseVisualStyleBackColor = true;
+            // 
+            // check_123
+            // 
+            resources.ApplyResources(this.check_123, "check_123");
+            this.check_123.Name = "check_123";
+            this.check_123.UseVisualStyleBackColor = true;
+            // 
+            // check_ask
+            // 
+            resources.ApplyResources(this.check_ask, "check_ask");
+            this.check_ask.Name = "check_ask";
+            this.check_ask.UseVisualStyleBackColor = true;
+            // 
+            // btn_generate
+            // 
+            resources.ApplyResources(this.btn_generate, "btn_generate");
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // trackBar1
+            // 
+            resources.ApplyResources(this.trackBar1, "trackBar1");
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 4;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Value = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll_1);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Name = "label1";
+            // 
+            // lbl_length
+            // 
+            resources.ApplyResources(this.lbl_length, "lbl_length");
+            this.lbl_length.Name = "lbl_length";
+            // 
+            // txt_password
+            // 
+            resources.ApplyResources(this.txt_password, "txt_password");
+            this.txt_password.Name = "txt_password";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.lbl_length);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.check_ask);
+            this.Controls.Add(this.check_123);
+            this.Controls.Add(this.check_abcd);
+            this.Controls.Add(this.check_ABC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.length);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label length;
+        private System.Windows.Forms.CheckBox check_ABC;
+        private System.Windows.Forms.CheckBox check_abcd;
+        private System.Windows.Forms.CheckBox check_123;
+        private System.Windows.Forms.CheckBox check_ask;
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_length;
+        private System.Windows.Forms.TextBox txt_password;
+    }
+}
+
---
+++ key_Generator/key.Generator/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace key.Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void trackBar1_Scroll_1(object sender, EventArgs e)
+        {
+            lbl_length.Text=trackBar1.Value.ToString();
+        }
+
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+
+            if (check_ABC.Checked == false && check_abcd.Checked == false && check_123.Checked == false && check_ask.Checked == false)
+            {
+                MessageBox.Show("Bütün seçenekler boş olamaz", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                txt_password.Text = string.Empty;
+                Random rnd = new Random();
+                string ABC = "QWERTYUIOPASDFGHJKLZXCVBNM";
+                string abc = "qwertyuıopasdfghjklzxcvbnm";
+                string number = "0123456789";
+                string special = "!+%=?*-_,.;$@";
+                string password = "";
+                if (check_ABC.Checked)
+                {
+                    password += ABC;
+                }
+                if (check_abcd.Checked)
+                {
+                    password += abc;
+                }
+                if (check_123.Checked)
+                {
+                    password += number;
+                }
+                if (check_ask.Checked)
+                {
+                    password += special;
+                }
+                for (int i = 0; i <= trackBar1.Value; i++)
+                {
+                    txt_password.Text += password[rnd.Next(password.Length)];
+                }
+            }
+        }
+        
+    }
+}
